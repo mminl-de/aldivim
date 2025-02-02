b09fbc6bc9fd53218f6995b8592c155c31a67da5
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-02-02 18:45:22 +0100

    opt.lua

--- lua/aldivim/opt.lua
+++ lua/aldivim/opt.lua
@@ -1,1 +1,34 @@
-print "TOOD opt.lua"
+local vim = vim
+local o = vim.opt
+
+-- line numbers
+o.number = true
+o.relativenumber = true
+
+-- tabs
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+
+-- wrap
+o.wrap = true
+o.linebreak = true
+
+-- colorscheme
+vim.cmd.colorscheme "catppuccin-mocha"
+
+-- backups
+o.swapfile = false
+o.backup = false
+
+-- scrolloff
+o.scrolloff = 29
+
+-- rulers
+o.colorcolumn = { 80, 90 }
+
+-- For some reason, nvim autoformats Zig code out of the box.
+-- This disables it.
+vim.g.zig_fmt_autosave = 0
