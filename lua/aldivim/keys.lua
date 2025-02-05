55344493758cc521cbfddc957d48bc4c536a45f3
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-02-05 15:48:10 +0100

    Simplified single-mode keybindgs

--- lua/aldivim/keys.lua
+++ lua/aldivim/keys.lua
@@ -4,16 +4,16 @@ local ks = vim.keymap.set
 vim.g.mapleader = " "
 
 -- basic
-ks({ "n" }, "<esc>", vim.cmd.nohlsearch) -- Esc in normal mode removes search highlights
+ks("n", "<esc>", vim.cmd.nohlsearch) -- Esc in normal mode removes search highlights
 
 -- editing
-ks({ "i" }, "<m-i>", "<esc>") -- Alt-i goes back to normal mode
-ks({ "i" }, "<m-backspace>", "<c-w>") -- delete the last word with Alt-Backspace
+ks("i", "<m-i>", "<esc>") -- Alt-i goes back to normal mode
+ks("i", "<m-backspace>", "<c-w>") -- delete the last word with Alt-Backspace
 
 -- writing
-ks({ "n" }, "<leader><leader>", vim.cmd.write) -- Space-Space saves the file
-ks({ "n" }, "<leader>z", vim.cmd.wq) -- Space-z saves and quits, like ZZ
-ks({ "n" }, "<leader>q", ":q!<cr>") -- Space-q quits without saving
+ks("n", "<leader><leader>", vim.cmd.write) -- Space-Space saves the file
+ks("n", "<leader>z", vim.cmd.wq) -- Space-z saves and quits, like ZZ
+ks("n", "<leader>q", ":q!<cr>") -- Space-q quits without saving
 
 -- snippets
 ks({ "i", "s" }, "<c-f>", function() vim.snippet.jump(1) end) -- jump between snippet placeholders Emacs-style
