4f85eaec6b7285a58dd2d1082034d369d7f148b6
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-02-07 13:47:16 +0100

    Some keys

--- lua/aldivim/keys.lua
+++ lua/aldivim/keys.lua
@@ -15,6 +15,40 @@ ks("n", "<leader><leader>", vim.cmd.write) -- Space-Space saves the file
 ks("n", "<leader>z", vim.cmd.wq) -- Space-z saves and quits, like ZZ
 ks("n", "<leader>q", ":q!<cr>") -- Space-q quits without saving
 
+-- buffers
+ks("n", "<leader>x", vim.cmd.bdelete)
+ks("n", "<leader><tab>", vim.cmd.bnext)
+ks("n", "<leader><s-tab>", vim.cmd.bprev)
+
+-- panes
+for _, d in ipairs { "h", "j", "k", "l" } do
+    ks({ "n", "i" }, "<m-" .. d .. ">", function() vim.cmd.wincmd(d) end)
+end
+
+ks("n", "<leader>j", function()
+    vim.cmd.split()
+    vim.cmd.wincmd "j"
+end)
+
+ks("n", "<leader>l", function()
+    vim.cmd.vsplit()
+    vim.cmd.wincmd "l"
+end)
+
+-- resizing panes
+ks("n", "<m-<>", function() vim.cmd "vertical resize -8" end)
+ks("n", "<m-s-<>", function() vim.cmd "vertical resize +8" end)
+ks("n", "<m-->", function() vim.cmd "vertical resize -4" end)
+ks("n", "<m-+>", function() vim.cmd "vertical resize +4" end)
+
 -- snippets
 ks({ "i", "s" }, "<c-f>", function() vim.snippet.jump(1) end) -- jump between snippet placeholders Emacs-style
 ks({ "i", "s" }, "<c-b>", function() vim.snippet.jump(-1) end)
+
+-- telescope
+local telescope = require "telescope"
+local builtin = require "telescope.builtin"
+ks("n", "<leader>b", builtin.buffers)
+ks("n", "<leader>e", telescope.extensions.file_browser.file_browser)
+ks("n", "<leader>f", builtin.find_files)
+ks("n", "<leader>t", builtin.colorscheme)
