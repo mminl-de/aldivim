9e71cc268000da38fb9f9da54c5d882afb9b7d2d
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-08-08 01:21:51 +0200

    Changed LSP icons

--- m4/lazy.m4.lua
+++ m4/lazy.m4.lua
@@ -205,13 +205,20 @@ require "lazy".setup {
 			vim.diagnostic.config({
 				virtual_text = {
 					spacing = 2,
-					prefix = "●",
+					prefix = "󱈸",
 				},
 				float = {
 					source = "always",
 					border = "rounded"
 				},
-				signs = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "",
+						[vim.diagnostic.severity.WARN] = "",
+						[vim.diagnostic.severity.HINT] = "",
+						[vim.diagnostic.severity.INFO] = "",
+					}
+				},
 				underline = true,
 				update_in_insert = false,
 			})
